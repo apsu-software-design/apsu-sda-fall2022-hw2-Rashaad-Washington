6b16ec738d34fe6fa55b48bb57861c7c80a24f2e
Rashaad H. Washington <washington_rashaad@yahoo.com>
2022-09-20 23:47:26 -0500

    Create members.ts

---
+++ src/members.ts
@@ -0,0 +1,20 @@
+import {Member} from './member';
+export class Members{
+    private members : Member[];
+
+    constructor(){
+        this.members = [];
+    }
+
+    public getMembers(){
+
+    }
+
+    findGatheringNames(query: string): string[] { 
+        return ;
+    }
+
+    addMember(name: string, email: string) {
+        return ;
+    }
+}
