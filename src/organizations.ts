0e4a54ccf24abc08f15395cf3e2a7b5076f5f8bf
Rashaad H. Washington <washington_rashaad@yahoo.com>
2022-09-20 23:48:20 -0500

    Create organizations.ts

---
+++ src/organizations.ts
@@ -0,0 +1,16 @@
+import {Organization} from './organization';
+
+export class Organizations{
+    private organizations : Organization[];
+
+    constructor(){
+        this.organizations = [];
+    }
+
+    addGatheringToOrganization(gatheringTitle: string, organizationTitle: string) {
+    }
+
+    findOrganizationNames(query: string): string[] {
+        return ;
+    }
+}
